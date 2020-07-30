eeaab84e537062bfb3f04f114e64302a1a3565e6
Oliver Malham <oliver.malham@gmail.com>
2020-07-30 20:30:02 +0100

    Significant progress on motion controller, updated example json packet

--- motion.py
+++ motion.py
@@ -4,17 +4,26 @@ import os
 import errno
 import json
 import serial
-
-from motion_lib.motors import WheelIO
+from time import sleep
+import servo_lib.lewansoul_lx16a
 
 BASE_DIR = '/etc/marvin/'
 MOTION_FIFO = 'motion'
+
+# UART serial port for servo bus
+SERIAL_PORT = '/dev/serial0'
 
 # USB serial ports for Arduino Nanos that control left and right side motors
 left_motor_path = '/dev/ttyUSB0'
 right_motor_path = '/dev/ttyUSB1'
 
 valid_attributes = ["wheel", "head"]
+
+servo_controller = servo_lib.lewansoul_lx16a.ServoController(
+        serial.Serial(SERIAL_PORT, 115200, timeout=0.2), 
+        timeout=0.5
+)
+
 
 """
 Note that the wheels are numbered front to back, odd on the left, even on the right.
@@ -27,6 +36,53 @@ We're assuming that the wheel servo IDs match the numbering described above. Hea
 """
 
 
+def update_motors(command):
+    """ Send motor control commands to the Ardunio Nanos
+    """
+    command_left = ""
+    command_right = ""
+
+    for index, wheel in enumerate(command["wheel"]):
+        if index % 2:
+            command_left = f"{command_left},{wheel['distance']},{wheel['speed']}"
+        else:
+            command_right = f"{command_right},{wheel['distance']},{wheel['speed']}"
+
+    command_left = command_left[1:]
+    command_right = command_right[1:]
+
+    print(f"Motors left: {command_left}")
+    print(f"Motors right: {command_right}")
+
+
+def update_servos(command):
+    """ Send servo commands
+    """
+    for index, wheel in enumerate(command["wheel"]):
+        position = int(wheel["angle"])
+        servo_id = index + 1
+        print(f"Servo {servo_id}: {position}; ", end='')
+        servo_controller.move(servo_id, position)
+    print('')
+        
+
+def servos_ready(command):
+    """ Return true is all the servos are within an error factor
+    """
+
+    for index, wheel in enumerate(command["wheel"]):
+        position = servo_controller.get_position(index + 1)
+        if abs(position - wheel["angle"]) > servo_error:
+            return False
+    return True
+
+
+def motors_ready(command):
+    """ Return true if the motors have moved the target distance
+    """
+    return True
+
+
 def valid_command(command):
     """ Check the received JSON matches what we expect
     """
@@ -36,24 +92,29 @@ def valid_command(command):
     return True
 
 
-while True:
+if __name__ == "__main__":
     print(f"Listening to motion FIFO on {BASE_DIR}{MOTION_FIFO}...")
-    with open(BASE_DIR+MOTION_FIFO) as fifo:
-        print(f"FIFO opened")
-        while True:
-            data = fifo.read()
-            if len(data) == 0:
-                print("Client disconnected")
-                break
-            try:
-              command = json.loads(data)
-              print(f'Command: {command}')
-              if not valid_command(command):
-                  print("Invalid command!")
-              update_motors(command)
-              
-            except Exception as e:
-              print("Error reading JSON packet")
-              print(e)
-              # Need better error handling!
-              
+    while True:
+        with open(BASE_DIR+MOTION_FIFO) as fifo:
+            print(f"FIFO opened")
+            while True:
+                data = fifo.read()
+                if len(data) == 0:
+                    print("Client disconnected")
+                    print("")
+                    break
+                try:
+                    command = json.loads(data)
+                    if not valid_command(command):
+                        print("Invalid command!")
+                    update_motors(command)
+                    update_servos(command)
+                    while not servos_ready(command) and not motors_ready(command):
+                        sleep(0.2)
+
+                except Exception as e:
+                    print("Error reading JSON packet")
+                    print(repr(e))
+                    # Need better error handling!
+            
+
