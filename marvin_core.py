1e1d0a5e8b294725a62ef594b9bd4200beb55384
om <oliver.malham@gmail.com>
2021-09-19 20:38:39 +0100

    Implemented more of the movement commands

--- marvin_core.py
+++ marvin_core.py
@@ -1,11 +1,10 @@
-import os
-import errno
 import json
 import serial
 from time import sleep
 import servo_lib.lewansoul_lx16a
-from typing import List, NamedTuple
+from typing import List
 from dataclasses import dataclass
+from pprint import pprint
 
 
 @dataclass
@@ -35,12 +34,15 @@ class MarvinCore:
     # Allowable absolute difference between commanded position and actual position when stopped
     servo_tolerance = 10
 
-    currentState = MarvinState(servos=[ServoState()]*6, motors=[MotorState()]*6, sensors=[])
-    targetState = MarvinState(servos=[ServoState()]*6, motors=[MotorState()]*6, sensors=[])
-
+    # Hard coded for now - angle the steering wheels need to be at in order to turn on the spot.
+    turn_angle = 45.0  # TODO: Calculate!
 
     def __init__(self, servoController):
         self.servoController = servoController
+        self.currentState = MarvinState(servos=[ServoState(), ServoState(), ServoState(), ServoState(), ServoState(), ServoState()], 
+                                       motors=[MotorState(), MotorState(), MotorState(), MotorState(), MotorState(), MotorState()], sensors=[])
+        self.targetState = MarvinState(servos=[ServoState(), ServoState(), ServoState(), ServoState(), ServoState(), ServoState()], 
+                                       motors=[MotorState(), MotorState(), MotorState(), MotorState(), MotorState(), MotorState()], sensors=[])
 
     def execute(self, commandString):
         """ Executes the json encoded command string. Uses introspection, requires action names in the JSON to
@@ -53,21 +55,56 @@ class MarvinCore:
 
     def action_move(self, distance, speed):
         print(f"Action Move: distance {distance}, speed: {speed}")
+
         for motor in self.targetState.motors:
             motor.distance = distance
             motor.speed = speed
+
         for servo in self.targetState.servos[:4]:
             servo.angle = 0.0
+
         self.update_servos()
         self.update_motors()
 
     def action_turn(self, angle, speed):
-        # TODO: Finish me!
         print(f"Action Turn: angle {angle}, speed: {speed}")
 
+        for motor in self.targetState.motors:
+            motor.distance = 0.0
+            motor.speed = speed
+
+        # TODO: Need to calculate all of this, after measuring wheel base etc
+        self.targetState.motors[0].distance = 0
+        self.targetState.motors[0].speed = 0
+
+        self.targetState.motors[1].distance = 0
+        self.targetState.motors[1].speed = 0
+
+        self.targetState.motors[2].distance = 0
+        self.targetState.motors[2].speed = 0
+
+        self.targetState.motors[3].distance = 0
+        self.targetState.motors[3].speed = 0
+
+        self.targetState.motors[4].distance = 0
+        self.targetState.motors[4].speed = 0
+
+        self.targetState.motors[5].distance = 0
+        self.targetState.motors[5].speed = 0
+
+        self.targetState.servos[0].angle = self.turn_angle
+        self.targetState.servos[1].angle = -self.turn_angle
+        self.targetState.servos[2].angle = self.turn_angle
+        self.targetState.servos[3].angle = -self.turn_angle
+
+        self.update_servos()
+        self.update_motors()
+
     def action_head(self, pitch, yaw):
-        # TODO: Finish me!
         print(f"Action Head: pitch {pitch}, yaw: {yaw}")
+        self.targetState.servos[4] = yaw
+        self.targetState.servos[5] = pitch
+        self.update_servos()
 
     def action_action(self, command):
         # TODO: Finish me!
@@ -94,24 +131,14 @@ class MarvinCore:
     def update_servos(self):
         """ Send servo commands so that the current state matches the target state
         """
-
-        self.servoController.move(1, self.targetState.servos[0].angle)
-        sleep(0.02)
-        self.servoController.move(2, self.targetState.servos[1].angle)
-        sleep(0.02)
-        self.servoController.move(3, self.targetState.servos[2].angle)
-        sleep(0.02)
-        self.servoController.move(4, self.targetState.servos[3].angle)
-        sleep(0.02)
-        self.servoController.move(5, self.targetState.servos[4].angle)
-        sleep(0.02)
-        self.servoController.move(6, self.targetState.servos[5].angle)
-        sleep(0.02)
+        print(f"Target state:\n {self.targetState}")
+        # for servo in range[6]:
+        #     self.servoController.move(servo, self.targetState.servos[servo].angle)
+        #     sleep(0.2)
 
     def servos_ready(self):
         """ Return true if all the servos are within an error factor
         """
-
         sleep(0.1)
         position = self.servoController.get_position(1)
         if abs(position - self.targetState.servos[0].angle) > self.servo_tolerance:
